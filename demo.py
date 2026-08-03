09a3158dc036ffa89a38ec3d961fa3b57b80f57a
KzRaihan <kamruzzamanraihan00@gmail.com>
2026-08-03 17:11:05 +0600

    add exception

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
-from us_visa.logger import logging
+from  us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Logging is configured and ready to use.")
+try:
+    a = 1 / 0
+
+except Exception as e:
+    # also store in logger file
+    logging.error("An error occurred: %s", str(e))
+
+    raise USvisaException(e, sys)
