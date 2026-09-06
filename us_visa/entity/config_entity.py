81c2bd2082c9999f9122c74f8084dfd463fcc392
KzRaihan <kamruzzamanraihan00@gmail.com>
2026-09-07 00:23:38 +0600

    prediction pipeline added

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -217,3 +217,12 @@ class ModelPusherConfig:
     # --------------------------------------------------------------------------
     s3_model_key_path: str = MODEL_FILE_NAME
 
+
+# ==============================================================================
+# PREDICTION PIPELINE CONFIGURATION
+# ==============================================================================
+@dataclass
+class USvisaPredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
+
